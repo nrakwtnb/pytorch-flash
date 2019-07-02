370184aab1c8425337c1d0516edc4a6f1f7dd829
Noriaki Watanabe <noriaki.mathphys@gmail.com>
2019-07-02 23:18:28 +0900

    add improved fowrad_wrapper (trial)

--- dev.py
+++ dev.py
@@ -18,6 +18,7 @@ def forward_wrap(func):
     def _forward_wrap(self, inputs, target_device=None, retain_comp_graph=True):
         # this is only valid for the case where model is put on a single device
         device = next(self.parameters()).device
+<<<<<<< Updated upstream
         outputs = func(self, { k:v.to(device) for k,v in inputs.items() })
         if retain_comp_graph:
             outputs = outputs.detach()
@@ -26,11 +27,44 @@ def forward_wrap(func):
         else:
             # exception case needed if cannot be passed onto the device
             return { k:v.to(target_device) for k,v in outputs.items() }
+=======
+        outputs = func(self, _apply_transform(inputs, device=device))
+        if (target_device is None or target_device == device) and retain_comp_graph:# assume the single device for the model too
+            return outputs
+        else:
+            # exception case needed if cannot be passed onto the device
+            return _apply_transform(outputs, device=target_device, retain_comp_graph=retain_comp_graph)
+    return _forward_wrap
+
+
+def forward_wrap_imp(func):
+    @functools.wraps(func)
+    def _forward_wrap(self, inputs, target_device=None, retain_comp_graph=True, update_info=None, **kwargs):
+        # this is only valid for the case where model is put on a single device
+        device = next(self.parameters()).device# self.device ...
+        if update_info is not None:
+            Is_updated = self.name in update_info.get('updated_models', [self.name])
+            Is_computed = self.name in update_info.get('computed_models', [self.name])
+            if not Is_computed:
+                return {}
+            if not Is_updated:
+                retain_comp_graph = False
+        # > outputs = func(self, { k:v.to(device) for k,v in inputs.items() })
+        outputs = func(self, _apply_transform(inputs, device=device), **kwargs)
+        # > if retain_comp_graph:
+        # >    outputs = outputs.detach()
+        if (target_device is None or target_device == device) and retain_comp_graph:# assume the single device for the model too
+            return outputs
+        else:
+            # exception case needed if cannot be passed onto the device
+            # > return { k:v.to(target_device) for k,v in outputs.items() }
+            return _apply_transform(outputs, device=target_device, retain_comp_graph=retain_comp_graph)
+>>>>>>> Stashed changes
     return _forward_wrap
 
 
 def input_default_wrapper(batch):
-    return {"x":batch[0]},{"y":batch[1]}
+    return {"x":batch[0]}, {"y":batch[1]}
 
 
 
@@ -165,7 +199,7 @@ def create_trainer(model, optimizer, loss_fn, device=None, non_blocking=False, d
 
         inputs, labels = input_transform(batch)
         outputs = model(inputs)
-        loss = loss_fn({"inputs" : inputs,"outputs":outputs, "labels":labels}) / grad_accumulation_steps
+        loss = loss_fn({"inputs" : inputs,"outputs":outputs, "labels":labels}) * loss_weight
         loss.backward()
 
         if Is_update:
