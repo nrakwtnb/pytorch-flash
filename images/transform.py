ba423436734698bdd976389af36ee0b1c239f826
Noriaki Watanabe <noriaki.mathphys@gmail.com>
2019-09-01 21:44:54 +0900

    update data augmentation

--- images/transform.py
+++ images/transform.py
@@ -15,8 +15,27 @@ DA_default_config = {
     "Affine" : { "shift_limit" : 0.0625, "scale_limit" : 0.1, "rotate_limit" : 45 },
 }
 
-def get_image_transformer(DA_config=DA_default_config, p=1.0):
-    transformer = Compose([ DA_name2func[k](**v) for k,v in sorted(DA_config.items(), key=lambda x:x[0])]+[ToTensor()], p=p)
+def get_image_transformer(DA_config=DA_default_config, convert_to_tensor=True, p=1.0):
+    transformer = Compose([ DA_name2func[k](**v) for k,v in sorted(DA_config.items(), key=lambda x:x[0])], p=p)
+    if convert_to_tensor:
+        transformer = transformer + [ToTensor()]
     def get_image_transformer_(image):
         return transformer(image=image)['image']
     return get_image_transformer_
+
+def get_filter_transformer(DA_config=DA_default_config, convert_to_tensor=True, p=1.0):
+    transformer = Compose([ DA_name2func[k](**v) for k,v in sorted(DA_config.items(), key=lambda x:x[0])], p=p)
+    if convert_to_tensor:
+        transformer = transformer + [ToTensor()]
+    def get_filter_transformer_(image, mask):
+        aug = transformer(image=image, mask=mask)
+        return aug['image'], aug['mask']
+    return get_filter_transformer_
+
+"""
+def albumentation_wrapper(image_transformer):
+    def augmentator(image, mask):
+        aug = image_transformer(image=image, mask=mask)
+        return aug['image'], aug['mask']
+    return augmentator
+"""
